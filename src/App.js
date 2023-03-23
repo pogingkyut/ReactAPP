92936fb834c36c9425c0df3252f4acf587ab97e7
Randolf Santiago <randolf_santiago85@yahoo.com>
2023-03-23 12:12:32 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import ArticleList from "./components/ArticleList";
+import Form from "./components/Form";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [editArticle, setEditArticle] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/articles", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token 4e3581a185c56181e912b746f89f2ef9fec5c043",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setArticles(resp))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const editBtn = (article) => {
+    setEditArticle(article);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Django-React App</h1>
+      <ArticleList articles={articles} editBtn={editBtn} />
+      {editArticle ? <Form editArticle={editArticle} /> : null}
     </div>
   );
 }
